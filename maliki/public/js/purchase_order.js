5068b590a2691ee74a56e60c688398ae87448504
Madcheese101 <40042269+Madcheese101@users.noreply.github.com>
2025-03-04 18:31:49 +0200

    populate required by field at load

--- maliki/public/js/purchase_order.js
+++ maliki/public/js/purchase_order.js
@@ -6,7 +6,13 @@ frappe.ui.form.on('Purchase Order', {
         if(!frm.doc.__islocal){
             return;
         }
-
-        frm.set_value('set_warehouse', settings["ly_warehouse"]);
+        if(!frm.doc.set_warehouse){
+            frm.set_value('set_warehouse', settings["ly_warehouse"]);
+        }
+        if(!frm.doc.schedule_date){
+            let dateString = frm.doc.transaction_date;
+            frm.set_value('schedule_date', frappe.datetime.add_days(dateString, 3));
+        }
+        
 	}
 })
